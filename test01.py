b0b6d8f0372d249603636a33cf030d0b6bb580c7
cjlim <cjlim000523@gmail.com>
2025-09-17 13:29:06 +0900

    modify test01.py by

--- test01.py
+++ test01.py
@@ -1,7 +1,8 @@
 def addNum(a,b):
     sum = a + b
     return sum
-num 01 = 10
+num 01 = 100
 num 02 = 20
-print(addNum(num01, num02)
+print(addNum(num01, num02))
 print("Program was ended")
+print("Bye")
